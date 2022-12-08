502e2440371dbe3a02e5a82a534c6fd93e935bc0
lauromiottojr <junior.miotto36@gmail.com>
2022-12-08 17:34:59 -0300

    POST Todo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import styles from './App.module.css';
 import { useState, useEffect } from 'react';
 import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from 'react-icons/bs'
 
-const API = "htpp://localhost:5000";
+const API = "http://localhost:5000";
 
 function App() {
 
@@ -12,19 +12,57 @@ function App() {
   const [todos, setTodos] = useState([])
   const [loading, setLoading] = useState(false)
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false
+    };
+    await fetch(API + '/todos', {
+      method: "POST",
+      body: JSON.stringify(todo),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    setTitle('')
+    setTime('')
+  }
 
 
 
   return (
     <div className={styles.app}>
+
       <div className={styles.todoHeader}>
         <h1>Todo</h1>
       </div>
+
       <div className={styles.todoForm}>
-        <p>form</p>
+        <h2>Insira sua próxima tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+          <div className={styles.formControl}>
+            <label htmlFor='title'>O que você irá fazer?</label>
+            <input type="text" name="title" placeholder='Titulo da tarefa'
+              onChange={(e) => setTitle(e.target.value)} value={title || ''} required />
+          </div>
+
+          <div className={styles.formControl}>
+            <label htmlFor='time'>Duração:</label>
+            <input type="text" name="time" placeholder='Tempo estimado (em horas)'
+              onChange={(e) => setTime(e.target.value)} value={time || ''} required />
+          </div>
+
+          <input type="submit" value="Criar tarefa" />
+
+        </form>
       </div>
-      <div className={styles.todoForm}>
-        <p>lista</p>
+
+      <div className={styles.todoList}>
+        <h2>list</h2>
+        {todos.length === 0 && <p>Não há tarefas!</p>}
       </div>
     </div>
   );
