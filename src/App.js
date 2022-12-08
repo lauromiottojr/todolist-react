ada459863f0afc1f9881c9029fa5832d52d57f18
lauromiottojr <junior.miotto36@gmail.com>
2022-12-08 15:02:50 -0300

    App styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
+import styles from './App.module.css';
+
+import { useState, useEffect } from 'react';
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from 'react-icons/bs'
+
+const API = "htpp://localhost:5000";
 
 function App() {
+
+  const [title, setTitle] = useState("")
+  const [time, setTime] = useState("")
+  const [todos, setTodos] = useState([])
+  const [loading, setLoading] = useState(false)
+
+
+
+
   return (
-    <div className="App">
-      <h1>Hello!</h1>
+    <div className={styles.app}>
+      <div className={styles.todoHeader}>
+        <h1>Todo</h1>
+      </div>
+      <div className={styles.todoForm}>
+        <p>form</p>
+      </div>
+      <div className={styles.todoForm}>
+        <p>lista</p>
+      </div>
     </div>
   );
 }
